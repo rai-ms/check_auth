80d66e3e6f13e338fd4b5030f496e21e73d786af
Ashish Rai <ashish.rai@appinventiv.com>
2024-03-26 13:26:17 +0530

    first commit

---
+++ lib/bio_metric_service.dart
@@ -0,0 +1,24 @@
+import 'package:local_auth/local_auth.dart';
+
+class LocalAuthenticationService {
+  final LocalAuthentication auth = LocalAuthentication();
+
+  init() async {
+    final bool canAuthenticateWithBiometrics = await auth.canCheckBiometrics;
+    final bool canAuthenticate = canAuthenticateWithBiometrics || await auth.isDeviceSupported();
+  }
+
+  getBio() async {
+    final List<BiometricType> availableBiometrics = await auth.getAvailableBiometrics();
+
+    if (availableBiometrics.isNotEmpty) {
+      // Some biometrics are enrolled.
+    }
+
+    if (availableBiometrics.contains(BiometricType.strong) ||
+        availableBiometrics.contains(BiometricType.face)) {
+      // Specific types of biometrics are available.
+      // Use checks like this with caution!
+    }
+  }
+}
